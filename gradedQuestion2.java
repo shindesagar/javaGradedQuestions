60178613e9d66ba8eb84d45effc1e0d1ef35ebbd
Sagar Shinde <s.sagar@outlook.com>
2023-05-19 16:48:12 +0530

    My java first graded question

---
+++ gradedQuestion2.java
@@ -0,0 +1,11 @@
+public class gradedQuestion2 {
+    public static void main(String[] args) { 
+        int num1 =  10;
+        int num2 = 800;
+        System.out.println(num1 += 2);
+        System.out.println(num1 -= 4);
+        System.out.println(num1 *= 5.6);
+        System.out.println(num2 /= num1);
+        System.out.println(num1 %= 3);
+    }
+}
