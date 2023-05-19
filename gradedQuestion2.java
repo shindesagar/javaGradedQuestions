b5d55d6cdd3ad21b3095d20ba17b989103d45ec6
Sagar Shinde <s.sagar@outlook.com>
2023-05-19 17:20:21 +0530

    Second Question revise code

--- gradedQuestion2.java
+++ gradedQuestion2.java
@@ -1,11 +1,20 @@
 public class gradedQuestion2 {
     public static void main(String[] args) { 
-        int num1 =  10;
-        int num2 = 800;
-        System.out.println(num1 += 2);
-        System.out.println(num1 -= 4);
-        System.out.println(num1 *= 5.6);
-        System.out.println(num2 /= num1);
-        System.out.println(num1 %= 3);
+        int num1 = 10;
+
+        num1 += 2;
+        System.out.println("After addition: num1 = " + num1);
+
+        num1 -= 4;
+        System.out.println("After subtraction: num1 = " + num1);
+
+        num1 *= 7;
+        System.out.println("After multiplication: num1 = " + num1);
+
+        num1 /= 4;
+        System.out.println("After division: num1 = " + num1);
+        
+        num1 %= 3;
+        System.out.println("After modulus: num1 = " + num1);
     }
 }
